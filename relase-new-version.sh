8b136f83ad0625f98c0cc82a4a74b58964646d66
Toma Luca <toma.luca95@gmail.com>
2015-09-29 16:41:16 +0000

    Minor fix in the relase script

--- relase-new-version.sh
+++ relase-new-version.sh
@@ -14,29 +14,37 @@ fi
 
 . ./factory.config
 
-OLD_W2L_INSTANCE_NAME=$(cat instances.log | tail -1)
+if [ -f instances.log ] ; then
+ OLD_W2L_INSTANCE_NAME=$(cat instances.log | tail -1)
 
-echo "Old version name: "$OLD_W2L_INSTANCE_NAME
-W2L_INSTANCE_NAME=$OLD_W2L_INSTANCE_NAME
-cd "${W2L_RUNNING_DIR}/${W2L_INSTANCE_NAME}/"
-echo "\$wgReadOnly = 'This wiki is currently being upgraded to a newer software version.';" >> LocalSettings.php
-cd "${W2L_RUNNING_DIR}/${W2L_INSTANCE_NAME}/Dockers"
-./backup.sh
+ echo "Old version name: "$OLD_W2L_INSTANCE_NAME
+ W2L_INSTANCE_NAME=$OLD_W2L_INSTANCE_NAME
+ cd "${W2L_RUNNING_DIR}/${W2L_INSTANCE_NAME}/"
+ echo "\$wgReadOnly = 'This wiki is currently being upgraded to a newer software version.';" >> LocalSettings.php
+ cd "${W2L_RUNNING_DIR}/${W2L_INSTANCE_NAME}/Dockers"
+ ./backup.sh
 
-cd "$FACTORY_PWD"
+ cd "$FACTORY_PWD"
 
-cd WikiToLearn
-export W2L_COMMIT=""
-if [[ "$W2L_STAGING" == "1" ]] ; then
- export W2L_COMMIT=$(git log -n1 | grep commit | awk '{ print $2 }')
+ cd WikiToLearn
+ export W2L_COMMIT=""
+ if [[ "$W2L_STAGING" == "1" ]] ; then
+  export W2L_COMMIT=$(git log -n1 | grep commit | awk '{ print $2 }')
+ else
+  export W2L_COMMIT=$(git show $(git tag | sort -Vr | head -1) | head -1 | grep commit | awk '{ print $2 }')
+ fi
+ cd ..
+
+ BACKUP_DIR=$(ls $W2L_BACKUP_PATH | sort -Vr | head -1)
+ echo "New version hash: "$W2L_COMMIT
+ echo "Backup dir: "$BACKUP_DIR
+
+ export W2L_BACKUP_TO_RESTORE=$W2L_BACKUP_PATH"/"$BACKUP_DIR
+ export W2L_INSTANCE_NAME=${W2L_COMMIT:0:8}
 else
- export W2L_COMMIT=$(git show $(git tag | sort -Vr | head -1) | head -1 | grep commit | awk '{ print $2 }')
+ if [[ "$1" != "" ]] ; then
+  echo "Restoring $1 backup..."
+  export W2L_BACKUP_TO_RESTORE="$1"
+ fi
 fi
-cd ..
-
-BACKUP_DIR=$(ls $W2L_BACKUP_PATH | sort -Vr | head -1)
-echo "New version hash: "$W2L_COMMIT
-echo "Backup dir: "$BACKUP_DIR
-
-export W2L_BACKUP_TO_RESTORE=$W2L_BACKUP_PATH"/"$BACKUP_DIR
 ./make-instance.sh
